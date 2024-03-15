66a2c6ed7a74096f15dc7db9b4df585d52c474d4
SARATH <sarath210685@gmail.com>
2024-03-15 23:48:54 +0530

    error handling

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const path=require("path");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError=require("./utils/ExpressError.js");
+
 main().then(()=>{
     console.log("connected to db");
 }).catch(err=>{
@@ -27,50 +30,47 @@ app.get("/",(req,res)=>{
 res.send("server is ");
 });
 
-app.get("/listings",async (req,res)=>{
+app.get("/listings",wrapAsync(async (req,res)=>{
  const allListings=await Listing.find({});
  res.render("./listings/index.ejs",{allListings});
-});
+}));
 
 app.get("/listings/new",(req,res)=>{
     res.render("./listings/new.ejs");
     });
 
-    app.get("/listings/:id", async (req, res) => {
+ app.get("/listings/:id", wrapAsync(async (req, res) => {
         let { id } = req.params;
-        try {
           const listing = await Listing.findById(id);
-          console.log("Listing found:", listing); // Add this line to check if listing is retrieved
+          console.log("Listing found:", listing); 
           res.render("./listings/show.ejs", { listing });
-        } catch (err) {
-          console.error("Error fetching listing:", err);
-          res.status(500).send("Error fetching listing");
-        }
-      });
+      })
+);
 
-app.post("/listings",async (req,res)=>{
+app.post("/listings",wrapAsync(async (req,res,next)=>{
     const newListing=new Listing(req.body.listing);
     await newListing.save();
     res.redirect("./listings");
-})
+}));
 
-app.get("/listings/:id/edit",async (req,res)=>{
+app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
     let {id}=req.params;
     const listing=await Listing.findById(id);
     res.render("./listings/edit.ejs",{listing});
-});
+}));
 
-app.put("/listings/:id", async (req, res) => {
+app.put("/listings/:id", wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, req.body.listing);
-  res.redirect(`/listings/${id}`); // Redirect to the updated listing's details page
-});
-  app.delete("/listings/:id",async (req,res)=>{
+  res.redirect(`/listings/${id}`); 
+}));
+
+  app.delete("/listings/:id",wrapAsync(async (req,res)=>{
     let { id } = req.params;
     let deletedListing=await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect("/listings");
-  });
+  }));
 
 // app.get("/testListing",async (req,res)=>{
 // let sampleListing=new Listing({
@@ -85,6 +85,14 @@ app.put("/listings/:id", async (req, res) => {
 // res.send("successful");
 // });
 
+app.all("*",(req,res,next)=>{
+  next(new ExpressError(404,"PAGE NOT FOUND"));
+});
+
+app.use((err,req,res,next)=>{
+  let {statusCode=500,message="something went wrong"}=err;
+  res.status(statusCode).send(message);
+});
 
 app.listen(8080,()=>{
     console.log("server is listening");
