8a4e9ce28755c6c6be3d6a6bbd256e5b33bbb2ad
SARATH <sarath210685@gmail.com>
2024-03-16 21:02:32 +0530

    error 4

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const methodOverride=require("method-override");
 const ejsMate=require("ejs-mate");
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
+const {listingSchema}=require("./schema.js");
+
 
 main().then(()=>{
     console.log("connected to db");
@@ -30,6 +32,16 @@ app.get("/",(req,res)=>{
 res.send("server is ");
 });
 
+const validateListing=(req,res,next)=>{
+  let {error}=listingSchema.validate(req.body);
+  if(error){
+    let errMsg=error.details.map((el)=>el.message).join(",");
+    throw new ExpressError(400,errMsg);
+  }else{
+    next();
+  }
+};
+
 app.get("/listings",wrapAsync(async (req,res)=>{
  const allListings=await Listing.find({});
  res.render("./listings/index.ejs",{allListings});
@@ -47,36 +59,8 @@ app.get("/listings/new",(req,res)=>{
       })
 );
 
-app.post("/listings",wrapAsync(async (req,res,next)=>{
-  if(!req.body.listing){
-    throw new ExpressError(400,"SEND VALID DATA FOR LISTING");
-  }
+app.post("/listings",validateListing,wrapAsync(async (req,res,next)=>{
     const newListing=new Listing(req.body.listing);
-
-    if(!newListing.title)
-    {
-      throw new ExpressError(400,"TITLE IS MISSING");
-    }
-    if(!newListing.description)
-    {
-      throw new ExpressError(400,"DECRIPTION IS MISSING");
-    }
-    if(!newListing.image)
-    {
-      throw new ExpressError(400,"IMAGE IS MISSING");
-    }
-    if(!newListing.price)
-    {
-      throw new ExpressError(400,"PRICE IS MISSING");
-    }
-    if(!newListing.country)
-    {
-      throw new ExpressError(400,"COUNTRY IS MISSING");
-    }
-    if(!newListing.location)
-    {
-      throw new ExpressError(400,"LOCATION IS MISSING");
-    }
     await newListing.save();
     res.redirect("./listings");
 }));
@@ -87,10 +71,7 @@ app.get("/listings/:id/edit",wrapAsync(async (req,res)=>{
     res.render("./listings/edit.ejs",{listing});
 }));
 
-app.put("/listings/:id", wrapAsync(async (req, res) => {
-  if(!req.body.listing){
-    throw new ExpressError(400,"SEND VALID DATA FOR LISTING");
-  }
+app.put("/listings/:id",validateListing, wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, req.body.listing);
   res.redirect(`/listings/${id}`); 
